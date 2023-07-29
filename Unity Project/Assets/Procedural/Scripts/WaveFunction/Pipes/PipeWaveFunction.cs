9b205ed2c74e5b38b00009468c011f44c4c96936
William Kolodziejski <william.kolodziejski@l2code.com.br>
2023-07-28 21:13:25 -0300

    feat: created pipe wave function using wave function class

--- Unity Project/Assets/Procedural/Scripts/WaveFunction/Pipes/PipeWaveFunction.cs
+++ Unity Project/Assets/Procedural/Scripts/WaveFunction/Pipes/PipeWaveFunction.cs
@@ -15,30 +15,11 @@ namespace Procedural.Scripts.WaveFunction.Pipes
 
     private WaveFunction<PipeWaveProperty> _waveFunction;
     private List<WaveState<PipeWaveProperty>> _states;
+    private int _x;
 
     private void Awake()
     {
       _states = new List<WaveState<PipeWaveProperty>>();
-
-      for (var i = 0; i < 10; i++)
-      {
-        for (var j = 0; j < 10; j++)
-        {
-          _states.Add(
-            new PipeWaveState(
-              _states,
-              new Vector2(i, j),
-              possibilities,
-              Instantiate(
-                pipePrefab,
-                new Vector3(i, j, 0),
-                Quaternion.identity
-              )
-            )
-          );
-        }
-      }
-
       _waveFunction = new WaveFunction<PipeWaveProperty>(_states, PositionUtils.ManhattanDistance);
     }
 
@@ -52,6 +33,25 @@ namespace Procedural.Scripts.WaveFunction.Pipes
 
     private void OnMouseDown()
     {
+      for (var i = 0; i < 10; i++, _x++)
+      {
+        for (var j = 0; j < 10; j++)
+        {
+          _states.Add(
+            new PipeWaveState(
+              _states,
+              new Vector2(_x, j),
+              possibilities,
+              Instantiate(
+                pipePrefab,
+                new Vector3(_x, j, 0),
+                Quaternion.identity
+              )
+            )
+          );
+        }
+      }
+      
       StartCoroutine(Draw());
     }
 
